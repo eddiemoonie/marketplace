e34bf83c280c7eb0eedd5ba43682e51358cfcf79
github username <github email address>
2018-11-05 16:08:51 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-
+  get "/login", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  
   resources :users
   resources :products
 end
