b3da94a9e46999739480e15401815de5622b6dd4
github username <github email address>
2018-11-08 15:22:36 -0500

    resize image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image
+  has_attached_file :image, :styles => {
+      :thumb    => ['100x100#',  :jpg, :quality => 70],
+      :preview  => ['480x480#',  :jpg, :quality => 70],
+      :large    => ['600>',      :jpg, :quality => 70],
+      :retina   => ['1200>',     :jpg, :quality => 30]
+    }
+
   validates_attachment :image,
                      content_type: { content_type: /\Aimage\/.*\z/ },
                      size: { less_than: 1.megabyte }
