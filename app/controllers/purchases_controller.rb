fdd13b6ed0dbfd52af82707211557fad25901628
github username <github email address>
2018-11-09 16:52:26 -0500

    purchasing product

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,15 @@
 class PurchasesController < ApplicationController
 
   def new
-    @purchase = Purchase.create(
+    @purchase = Purchase.new(
       :user_id => params[:user_id],
       :product_id => params[:product_id]
     )
+    if @purchase.save
+      @product = Product.find(@purchase.product_id)
+      @product.sold = true
+      @product.save
+    end
     redirect_to user_path(@purchase.user)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  :id => 1,
+  :username => "Brian",
+  :password => "shim",
+  :email => "brianshim@gmail.com"
+)
+
+User.create(
+  :id => 2,
+  :username => "Eddie",
+  :password => "moon",
+  :email => "eddiemoonie@gmail.com"
+)
+
+User.create(
+  :id => 3,
+  :username => "Kevin",
+  :password => "kwon",
+  :email => "kevinkwon@gmail.com"
+)
