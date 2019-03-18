82c73105df810148db6d8007dca5f0549ea226a0
github username <github email address>
2019-03-18 16:26:05 -0400

    post feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,19 +1,9 @@
 class FeedbacksController < ApplicationController
   # before_action :set_feedback, only: [:show, :edit, :update, :destroy]
 
-  # def index
-  #   @feedbacks = Feedback.find_by_user_id(params[:user_id])
-  # end
-  #
-  # def new
-  #   @feedback = Feedback.new
-  # end
-  #
   def create
     @feedback = Feedback.new(feedback_params)
-    @product = Product.find_by_id(params[:id])
     @feedback.buyer_id = current_user.id
-    @feedback.user_id = @product.id
     if @feedback.save
       redirect_to user_path(current_user)
     else
