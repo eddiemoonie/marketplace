c22a5eb1e609bf430e08ac7f0567a173e786a6d9
github username <github email address>
2019-07-03 11:56:31 -0400

    small fixes

--- app/controllers/favorited_items_controller.rb
+++ app/controllers/favorited_items_controller.rb
@@ -1,11 +1,10 @@
 class FavoritedItemsController < ApplicationController
-  
+
   def new
     @favorited_item = FavoritedItem.new(
       :favorites_list_id => params[:favorites_list_id],
       :product_id => params[:product_id]
     )
-    binding.pry
     if @favorited_item.save
       redirect_to "/favorites"
     else
